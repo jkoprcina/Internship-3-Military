49a9e871c0c35d962e0f1e4894e43c6891e449a9
jkoprcina <koprcina.josip@gmail.com>
2018-12-17 20:05:44 +0100

    Basic class logic started

---
+++ Military/Military/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Military
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Military/Military/Vehicle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace Military
+{
+    public abstract class Vehicle
+    {
+        private int _Id;
+        private int _Weight;
+        private int _AverageSpeed;
+        private int _FuelConsumption;
+        private int _Capacity;
+
+        public int Id { get => Id; set => _Id = value; }
+        public int Weight { get => Weight; set => _Weight = value; }
+        public int AverageSpeed { get => AverageSpeed; set => _AverageSpeed = value; }
+        public int FuelConsumption { get => FuelConsumption; set => _FuelConsumption = value; }
+        public int Capacity { get => Capacity; set => _Capacity = value; }
+
+        public Vehicle()
+        {
+        }
+
+        public virtual void Output()
+        {
+
+        }
+    }
+}
